f42abb8d7a0c49a4729afe10eb6072112a580ba2
David Adams <dadams@instructure.com>
2018-01-16 11:01:37 -0600

    v1.2: Add friendlier errors and identity parsing
    * Add capability for an explanatory error stack
    * Attempt to parse and display identities using aliases instead of ARNs

--- rapture.sh
+++ rapture.sh
@@ -16,6 +16,7 @@ fi
 declare -A _rapture
 declare -a _rapture_creds_stack
 declare -a _rapture_arn_stack
+declare -a _rapture_failures
 declare -A _rapture_alias
 declare -A _rapture_account
 declare -A _rapture_alias_r
@@ -29,11 +30,24 @@ _rapture_say() { _rapture_quiet || echo "rapture: $@"; }
 # 'msg' goes to stderr but can be quieted
 _rapture_msg() { _rapture_quiet || echo "rapture: $@" >&2; }
 
-# 'err' goes to stderr and ignores the quiet setting
-_rapture_err() { echo "rapture: ERROR: $@" >&2; }
+# 'err' goes to stderr and ignores the quiet setting and explains the failure
+_rapture_err() { echo "rapture: ERROR: $@" >&2; _rapture_explain_failure; }
+
+# save a stack of failure reasons for later explanation
+_rapture_failed_because() { _rapture_failures[${#_rapture_failures[@]}]="$1"; }
+
+# explain the reason for a failure, if possible, and clear the failure stack
+_rapture_explain_failure() {
+    local i=${#_rapture_failures[@]}
+    while (( i > 0 )); do
+        (( i-- ))
+        echo "rapture:  * ${_rapture_failures[${i}]}" >&2
+    done
+    _rapture_failures=()
+}
 
 _rapture_load_config() {
-    _rapture[VERSION]=1.1.0
+    _rapture[VERSION]=1.2.0
     _rapture[srcdir]=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
     _rapture[rootdir]="${RAPTURE_ROOT:-${_rapture[srcdir]}}"
     _rapture[sp]="${_rapture[sp]:-0}"
@@ -61,7 +75,7 @@ _rapture_load_config() {
               ((.identifier//"'"$USER"'")|tostring)
             ]|join(" ")' "${_rapture[config]}" )"
 
-    _rapture[managed_vars]="AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_SECURITY_TOKEN RAPTURE_ROLE"
+    _rapture[managed_vars]="AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_SECURITY_TOKEN RAPTURE_ROLE RAPTURE_ASSUMED_ROLE_ARN"
     _rapture[aws_cli_args]="--region ${_rapture[region]} --output json"
 
     ######################################################################
@@ -98,36 +112,39 @@ _rapture_load_config() {
 _rapture_load_config
 
 _rapture_api_sts_get_caller_identity() {
-    _rapture[sts_get_caller_identity_out]=$(
-        aws sts get-caller-identity \
-            ${_rapture[aws_cli_args]} \
-            2>/dev/null
-    )
-    if [[ -n ${_rapture[sts_get_caller_identity_out]} ]]; then
-        read _rapture[caller_account] \
-             _rapture[caller_userid] \
-             _rapture[caller_arn] \
-             <<< "$( jq -r '[.Account,.UserId,.Arn]|join(" ")' <<< "${_rapture[sts_get_caller_identity_out]}" )"
-        return 0
-    else
-        unset _rapture[sts_get_caller_identity_out]
-        unset _rapture[caller_account]
-        unset _rapture[caller_userid]
-        unset _rapture[caller_arn]
+    if ! _rapture_check_env; then
+        _rapture_failed_because "No AWS credential environment variables were found"
         return 1
     fi
-}
 
-_rapture_api_sts_get_session_token() {
-    local args=
-    if [[ ${_rapture[mfa_required]} == yes ]]; then
-        _rapture_prompt_for_mfa_otp
-        args="--serial-number ${_rapture[mfa_id]} --token-code ${_rapture[mfa_otp]}"
+    local api_out=$(
+        aws sts get-caller-identity \
+            ${_rapture[aws_cli_args]} \
+            2>&1
+    )
+
+    unset _rapture[caller_account]
+    unset _rapture[caller_userid]
+    unset _rapture[caller_arn]
+
+    # check that the return value is not empty
+    if [[ -z $api_out ]]; then
+        _rapture_failed_because "Call to sts:GetCallerIdentity gave no output"
+        return 1
     fi
 
-    ### TODO
-    echo "INCOMPLETE FUNCTION _rapture_api_sts_get_session_token" >&2
-    return 1
+    # check that the output is valid json
+    if ! jq . <<< "$api_out" &>/dev/null; then
+        _rapture_failed_because "Call to sts:GetCallerIdentity failed with:\n${api_out}"
+        return 1
+    fi
+
+    # parse the output into _rapture vars
+    read _rapture[caller_account] \
+         _rapture[caller_userid] \
+         _rapture[caller_arn] \
+         <<< "$( jq -r '[.Account,.UserId,.Arn]|join(" ")' <<< "$api_out" )"
+    return 0
 }
 
 _rapture_api_sts_assume_role() {
@@ -166,13 +183,6 @@ _rapture_api_sts_assume_role() {
         unset _rapture[assumed_expiration]
         return 1
     fi
-}
-
-_rapture_prompt_for_mfa_otp() {
-    unset _rapture[mfa_otp]
-    while [[ -z ${_rapture[mfa_otp]} ]]; do
-        read -p "Enter MFA Value: " _rapture[mfa_otp]
-    done
 }
 
 _rapture_parse_identity() {
@@ -242,7 +252,15 @@ _rapture_check_env() {
 
 _rapture_push() {
     # make sure current identity is cached before we push anything
-    [[ -n ${_rapture[identity_arn]} ]] || _rapture_parse_identity || return 1
+    if ! _rapture_parse_identity; then
+        _rapture_failed_because "Rapture was unable to parse the current identity"
+        return 1
+    fi
+
+    if [[ -z ${_rapture[identity_arn]} ]]; then
+        _rapture_failed_because "No identity ARN was set"
+        return 1
+    fi
 
     local encoded=$(
         for var in ${_rapture[managed_vars]}; do
@@ -281,6 +299,7 @@ _rapture_use_assumed_identity() {
     export AWS_SESSION_TOKEN="${_rapture[assumed_token]}"
     export AWS_SECURITY_TOKEN="${_rapture[assumed_token]}"
     export RAPTURE_ROLE="${_rapture[role_or_alias_to_assume]}"
+    export RAPTURE_ASSUMED_ROLE_ARN="${_rapture[assume_role_arn]}"
 
     _rapture_stash_env
     _rapture_parse_identity
@@ -293,7 +312,8 @@ _rapture_require_env() {
     fi
 
     if ! _rapture_check_stash; then
-        _rapture_err "AWS environment variables have changed. Rapture cannot safely operate."
+        _rapture_failed_because "AWS or rapture environment variables were changed outside of rapture."
+        _rapture_err "Rapture cannot safely operate."
         return 1
     fi
 }
@@ -343,6 +363,25 @@ _rapture_cmd_reload() {
     source ~/.rapture/rapture.sh
 }
 
+_rapture_friendly_description_of_current_principal() {
+    local account_name=${_rapture_account_r[${_rapture[identity_account]:--}]}
+    local alias_name=${_rapture_alias_r[${RAPTURE_ASSUMED_ROLE_ARN:--}]}
+
+    local friendly_account
+    if [[ -n $account_name ]]; then
+        friendly_account="'$account_name'"
+    else
+        friendly_account="${_rapture[identity_account]}"
+    fi
+
+    local friendly_description="${_rapture[identity_type]} ${_rapture[identity_name]} in account ${friendly_account}"
+    if [[ -n $alias_name ]]; then
+        echo "'$alias_name' ($friendly_description)"
+    else
+        echo "$friendly_description"
+    fi
+}
+
 _rapture_cmd_assume() {
     _rapture_require_env || return 1
     if [[ -n ${_rapture_alias[$1]} ]]; then
@@ -354,10 +393,10 @@ _rapture_cmd_assume() {
 
     if ! _rapture_api_sts_assume_role \
         || ! _rapture_use_assumed_identity; then
-        _rapture_err "could not assume role"
+        _rapture_err "Could not assume role"
         return 1
     fi
-    _rapture_msg "Assumed ${_rapture[identity_type]} ${_rapture[identity_name]} in account ${_rapture[identity_account]}"
+    _rapture_msg "Assumed $( _rapture_friendly_description_of_current_principal )"
 }
 
 _rapture_cmd_resume() {
@@ -366,7 +405,7 @@ _rapture_cmd_resume() {
         _rapture_err "Could not resume previous identity"
         return 1
     fi
-    _rapture_msg "Resumed ${_rapture[identity_type]} ${_rapture[identity_name]} in account ${_rapture[identity_account]}"
+    _rapture_msg "Resumed $( _rapture_friendly_description_of_current_principal )"
 }
 
 _rapture_cmd_stack() {
@@ -583,7 +622,7 @@ _rapture_cmd_init() {
 
     echo "Initializing vaulted env '$1':"
     eval "$( vaulted env "$1" )"
-    unset RAPTURE_ROLE
+    unset RAPTURE_ROLE RAPTURE_ASSUMED_ROLE_ARN
     _rapture[sp]=0
     _rapture_load_config
     _rapture_cmd_whoami
